ee591e4375d0811e34eea218d2b82af384ed34f5
André Marco Ruiz Sandoval <log1c.rs.psc@gmail.com>
2020-09-08 16:04:36 -0500

    Workspace finished

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import LandingPage from './views/LandingPage';
+
+ReactDOM.render(<LandingPage />, document.getElementById('root'));
---
+++ src/views/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  );
+};
+
+export default LandingPage;
