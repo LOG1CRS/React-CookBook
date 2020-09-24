d7fc37ee122bf3d43d67b6ffe9540c0f7370b617
André Marco Ruiz Sandoval <log1c.rs.psc@gmail.com>
2020-09-24 16:44:03 -0500

    unimportant line deleted

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { api_id, api_key } from '../api-keys.json';
 import HeroMain from '../components/main/HeroMain';
 import MainList from '../components/main/MainList';
 
 const LandingPage = () => {
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${api_id}&app_key=${api_key}`;
+  // const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${api_id}&app_key=${api_key}`;
 
   return (
     <>
